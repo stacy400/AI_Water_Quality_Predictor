7923636413e370360c74628f6fdad35fe22ee573
stacy400 <stacynjoga2@gmail.com>
2025-11-26 01:10:37 +0300

    .

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,8 +1,8 @@
 import joblib
 import os
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "../../ML/model.pkl")
-SCALER_PATH = os.path.join(os.path.dirname(__file__), "../../ML/scaler.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model.pkl")
+SCALER_PATH = os.path.join(os.path.dirname(__file__), "scaler.pkl")
 
 model = joblib.load(MODEL_PATH)
 scaler = joblib.load(SCALER_PATH)
